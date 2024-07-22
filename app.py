6a17233ebc9b41c534d2a88405f8c12c5e0eb98d
Andy Nguyen <andy@Andys-MacBook-Pro.local>
2024-07-21 21:46:35 -0700

    import AppBuilder in extensions file

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
+import logging
 from typing import Optional
 from flask import Flask
+import os
+
+from initialization import SupersetAppInitializer
 
 
+logger = logging.getLogger(__name__)
 def create_app(superset_config_module: Optional[str] = None) -> Flask:
     app = SupersetApp(__name__)
+
+    try:
+        config_module = superset_config_module or os.environ.get("SUPERSET_CONFIG_MODULE", "superset.config")
+        app.config.from_object(config_module)
+
+        app_initializer = app.config.get("APP_INITIALIZER", SupersetAppInitializer)(app)
+        app_initializer.init_app()
+
+        return app
+    except Exception as ex:
+        logger.exception("Fail to create app")
+        raise ex
 
 
 class SupersetApp(Flask):
---
+++ datasource/api.py
@@ -0,0 +1,3 @@
+from flask_appbuilder import AppBuilder
+
+app_builder = AppBuilder(update_perms=False)
