170d5e4e1c4282b81e03caa9502878123cb9d6f6
Andy Nguyen <andy@Andys-MacBook-Pro.local>
2024-07-21 23:24:54 -0700

    add base_api file

--- datasource/api.py
+++ datasource/api.py
@@ -1,3 +1,25 @@
-from flask_appbuilder import AppBuilder
+import logging
+from flask_appbuilder.api import expose, protect, safe
+logger = logging.getLogger(__name__)
+class DatasourceRestApi(BaseSupersetApi):
+    allow_browser_login = True
+    class_permission_name = "Datasource"
+    resource_name = "datasource"
+    openapi_spec_tag = "Datasources"
 
-app_builder = AppBuilder(update_perms=False)
+    @expose(
+            "/<datasource_type>/<int:datasource_id>/column/<column_name>/values/",
+            methods=("GET",),
+        )
+    @protect()
+    @safe
+    @statsd_metrics
+    @event_logger.log_this_with_context(
+        action=lambda self, *args, **kwargs: f"{self.__class__.__name__}"
+                                             f".get_column_values",
+        log_to_statsd=False,
+    )
+    def get_column_values(
+        self, datasource_type: str, datasource_id: int, column_name: str
+    ) -> FlaskResponse:
+        return self.response(200, result="Hello world")
---
+++ views/base_api.py
@@ -0,0 +1,19 @@
+import logging
+from typing import Any, Callable
+from flask_appbuilder.api import BaseApi
+
+logger = logging.getLogger(__name__)
+
+
+class BaseSupersetApiMixin:
+    csrf_exempt = False
+
+
+class BaseSupersetApi(BaseSupersetApiMixin, BaseApi):
+    ...
+
+
+def statsd_metrics(f: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Handle sending all statsd metrics from the REST API
+    """
