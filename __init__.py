2b3675801edec1584bd6c72bab02c6b4c0ebf42b
Andy Nguyen <andy@Andys-MacBook-Pro.local>
2024-07-24 18:19:12 -0700

    change endpoint in datasource/api.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import current_app, Flask
 from werkzeug.local import LocalProxy
 
-from app import create_app
-from extensions import (
-    appbuilder,
-    event_logger
-)
+# from app import create_app
+# from extensions import (
+#     appbuilder,
+#     event_logger
+# )
 
 app: Flask = current_app
--- initialization/__init__.py
+++ initialization/__init__.py
@@ -1,4 +1,5 @@
 from typing import Any, TYPE_CHECKING
+from flask import Flask
 
 from deprecation import deprecated
 from extensions import (appbuilder, APP_DIR)
@@ -8,18 +9,18 @@ if TYPE_CHECKING:
 
 
 class SupersetAppInitializer:
-    def __init__(self, app: SupersetApp) -> None:  # pylint: disable=too-many-public-methods
+    def __init__(self, app: Flask) -> None:  # pylint: disable=too-many-public-methods
         super().__init__()
 
         self.superset_app = app
         self.config = app.config
-
         self.manifest: dict[Any, Any] = {}
 
     @deprecated(details="use self.superset_app instead of self.flask_app")  # type: ignore
     @property
-    def flask_app(self) -> SupersetApp:
+    def flask_app(self) -> Flask:
         return self.superset_app
+
 
     def init_views(self) -> None:
         from datasource.api import DatasourceRestApi
