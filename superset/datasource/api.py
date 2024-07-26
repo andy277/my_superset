d125ce3a0c4218eb894f2a4bf44c6608ed338950
Andy Nguyen <andy@Andys-MacBook-Pro.local>
2024-07-26 07:46:19 -0700

    comment out variables

--- superset/datasource/api.py
+++ superset/datasource/api.py
@@ -7,12 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 class DatasourceRestApi(BaseSupersetApi):
-    allow_browser_login = True
-    class_permission_name = "Datasource"
+    # route_base = "/api"
+    # allow_browser_login = True
+    # class_permission_name = "Datasource"
     resource_name = "datasource"
-    openapi_spec_tag = "Datasources"
+    # openapi_spec_tag = "Datasources"
 
-    @expose("/superset")
+
+    @expose("/superset", methods=["GET"])
     def get_column_values(self) -> FlaskResponse:
         return self.response(200, result="Hello from Superset")
     # @protect()
